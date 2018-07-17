a2f249250c5caba5be24c3e88707910a498cea0b
Steve Chan <chanshunli@gmail.com>
2018-07-17 17:46:57 +0800

    OK: {"origin":"119.57.75.254"}

--- src_cljs/example/main.cljs
+++ src_cljs/example/main.cljs
@@ -1,5 +1,13 @@
-(ns example.main)
+(ns example.main
+  (:require [cljs.nodejs :as nodejs]
+            ["http" :as http]
+            ["request" :as request]
+            ["highland" :as highland]))
 
-; ^:export to keep this function during dead code elimination
+(nodejs/enable-util-print!)
+
 (defn ^:export main []
-  (println "App loaded!"))
+  (-> (request "https://httpbin.org/ip")
+      (.pipe (highland))
+      (.each (fn [output]
+                (print (.toString output "utf8"))))))
